c083a59f3e8ef501d21995699c88d1fddc8ae463
it21712 <59691614+it21712@users.noreply.github.com>
2022-02-17 17:05:16 +0200

    Update RestApiApplicationTests.java

--- src/test/java/gr/ds/restapi/RestApiApplicationTests.java
+++ src/test/java/gr/ds/restapi/RestApiApplicationTests.java
@@ -3,11 +3,11 @@ package gr.ds.restapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class RestApiApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//     @Test
+//     void contextLoads() {
+//     }
 
 }
